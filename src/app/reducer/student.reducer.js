18e99e7b10eeca3d2f290f3180250e5e16e2a070
Hoàng Như Sơn <101107014+hoangsson309@users.noreply.github.com>
2023-06-30 06:58:31 +0700

    Update student.reducer.js

--- src/app/reducer/student.reducer.js
+++ src/app/reducer/student.reducer.js
@@ -3,13 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [];
 
 export const StudentSlice = createSlice({
-    name: 'student',
-    initialState,
-    reducers: {
+    name: 'student', //tên
+    initialState, //giá trị khởi tạo
+    reducers: { //danh sách 
         SetStudent: (state, action) => {
-            state = action.payload;
+            state = action.payload; //Khi gọi API từ dòng 25 components/pages/home thì dữ liệu sẽ được truyền đến reducer, 
+                                    //và action.payload dùng để lấy ra dữ liệu, và cập nhật lại dữ liệu
+                                    //dispatch(SetStudent(response.data.content)); --gọi API
             return state;
-        },
+        }, 
+        //cái dưới đang suy nghĩ ==> bỏ qua
         SetResponseObject : (state, action) => {
             state = action.payload;
             return state;
