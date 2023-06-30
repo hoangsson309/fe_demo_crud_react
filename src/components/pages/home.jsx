1320463f3060f100f910a9d2614cb148cf15ccd6
Hoàng Như Sơn <101107014+hoangsson309@users.noreply.github.com>
2023-06-30 07:32:34 +0700

    Update home.jsx

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -6,11 +6,11 @@ import { StudentAPI } from '../../apis/studentAPI';
 import { ModalView } from './modal';
 
 const Home = () => {
-    const data = useAppSelector(GetStudent);
+    const data = useAppSelector(GetStudent); //lấy ra dữ liệu
     const dispatch = useAppDispatch();
-    const [current, setCurrent] = useState(1);
+    const [current, setCurrent] = useState(1); //(=== pageNo)
     const [student, setStudent] = useState({});
-    let [status, setStatus] = useState(false);
+    let [status, setStatus] = useState(false); //dùng để mở view Modal
     // let [listStudent, setListStudent] = useState([]);
     // let newStudent = useAppSelector(GetResponseObject);
     // let [count, setCount] = useState(false);
@@ -20,6 +20,8 @@ const Home = () => {
         fetchData();
     }, [current], [dispatch]);
 
+
+    //Gọi api getAll
     const fetchData = async () => {
         await StudentAPI.getAll(current - 1).then((response) => {
                 dispatch(SetStudent(response.data.content));
